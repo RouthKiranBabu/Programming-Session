ec979c358ea0cb436213595d85ba19f3d81f5468
Routh Kiran Babu <routhfamily123@gmail.com>
2024-06-08 21:52:08 +0530

    Create part3.py

---
+++ Programming/Python/OpenCV Module/Screen Recoder/part3.py
@@ -0,0 +1,18 @@
+import numpy as np
+import cv2
+from PIL import ImageGrab
+
+fourcc=cv2.VideoWriter_fourcc('X','V','I','D')
+vid=cv2.VideoWriter('record.avi',fourcc,0.1,(500,500))
+
+while True:
+	img=ImageGrab.grab(bbox=(100,10,600,500))
+	img_np=np.array(img)
+	vid.write(img_np)
+	cv2.imshow("frame",img_np)
+	key=cv2.waitKey(1)
+	if key==27:
+		break
+
+vid.release()
+cv2.destroyAllWindows()
