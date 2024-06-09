a143e0f29e20747b550f532d87a1db5be9fa926e
Routh Kiran Babu <51410158+RouthKiranBabu@users.noreply.github.com>
2024-06-09 07:42:25 +0530

    Delete Programming/Python/OpenCV Module directory

--- Programming/Python/OpenCV Module/My things/Vehicle prediction/Four wheelers/part1.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-from grabscreen import grab_screen
-import cv2
-
-#sys.path.append("..") 
-
-from utils import label_map_util
-from utils import visualization_utils as vis_util
