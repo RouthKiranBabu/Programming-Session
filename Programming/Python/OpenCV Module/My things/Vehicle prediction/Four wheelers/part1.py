08b9869f6caaad89cbc936c0e420886dbaf21368
Routh Kiran Babu <51410158+RouthKiranBabu@users.noreply.github.com>
2024-06-09 08:49:43 +0530

    Add files via upload
    Download by y2mate and save it in current folder.
    Video: https://youtu.be/mExFJdPHgC8

---
+++ Programming/Python/OpenCV Module/My things/Vehicle prediction/Four wheelers/part1.py
@@ -0,0 +1,8 @@
+from PIL import Image
+from grabscreen import grab_screen
+import cv2
+
+#sys.path.append("..") 
+
+from utils import label_map_util
+from utils import visualization_utils as vis_util
