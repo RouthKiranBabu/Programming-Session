a1f4906c86fca73dbc4acc3bbe94d7f0478eb0ec
Routh Kiran Babu <51410158+RouthKiranBabu@users.noreply.github.com>
2024-06-23 13:06:36 +0530

    Delete Courses directory

--- Courses/masai School(Joined 2024)/Self Learning Courses/Basics of Frontend Development Part1/09JavaScript/01Part/02Part.js
+++
@@ -1,9 +0,0 @@
-var StudentName = 'Routh';
-var studentAge = 26;
-var favFood = 'Protein Source';
-var hobby = 'Learning Exercise';
-var school = 'Masai school';
-var shoeSize = 5;
-var IndianCitizen = true;
-
-console.log(StudentName);
